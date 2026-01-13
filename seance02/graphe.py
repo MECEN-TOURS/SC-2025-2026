5213f0d8a560136cdfb9d02b02f912f84f9c8990
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2026-01-13 17:43:27 +0100

    seance 02 fin

--- seance02/graphe.py
+++ seance02/graphe.py
@@ -8,7 +8,8 @@ from typing import TypeVar
 T = TypeVar("T")
 
 
-def trouve_voisins(sommet: T, arretes: list[tuple[T, T]]) -> list[T]: ...
+def trouve_voisins(sommet: T, arretes: list[tuple[T, T]]) -> list[T]:
+    return [arrivee for (depart, arrivee) in arretes if depart == sommet]
 
 
 def sont_connectes(depart: T, arrivee: T, arretes: list[tuple[T, T]]) -> bool:
