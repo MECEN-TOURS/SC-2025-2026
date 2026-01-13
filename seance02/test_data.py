3c2d5456b89c861e9fe467dc1b561d62e454ee06
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2026-01-13 16:51:49 +0100

    seance 02 v3

--- seance02/test_data.py
+++ seance02/test_data.py
@@ -8,6 +8,12 @@ from data import (
     genere_arretes,
     genere_sommets,
 )
+import pytest
+
+
+def test_constantes():
+    assert DEPART == Etat.from_str("BLMC|")
+    assert ARRIVEE == Etat.from_str("|BLMC")
 
 
 def test_etat_to_str():
@@ -25,10 +31,34 @@ def test_Etat_from_str():
     assert attendu == Etat.from_str(entree)
 
 
+def test_Etat_from_str_bogue():
+    """vérifie que la méthode from_str plante pour des entrées non valides."""
+    with pytest.raises(ValueError):
+        Etat.from_str("blabla")
+    with pytest.raises(ValueError):
+        Etat.from_str("BL | MC")
+
+
 def test_est_valide():
     assert est_valide(Etat.from_str("|BLMC"))
     assert est_valide(Etat.from_str("BLMC|"))
     assert not est_valide(Etat.from_str("LM|BC"))
 
 
-def test_sont_connectes(): ...
+def test_sont_connectes():
+    assert sont_connectes(depart=Etat.from_str("BLMC|"), arrivee=Etat.from_str("LC|BM"))
+    assert not sont_connectes(
+        depart=Etat.from_str("BLMC|"), arrivee=Etat.from_str("BLMC|")
+    )
+    assert not sont_connectes(
+        depart=Etat.from_str("BLMC|"), arrivee=Etat.from_str("C|BLM")
+    )
+    assert not sont_connectes(
+        depart=Etat.from_str("BL|MC"), arrivee=Etat.from_str("LMC|B")
+    )
+
+
+def test_genere_sommets(): ...
+
+
+def test_genere_arretes(): ...
