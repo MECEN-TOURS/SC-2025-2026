5ada8f91b245c239e3a88ac35e8b818139e7d066
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2026-01-13 14:52:49 +0100

    seance 01 no 4

--- seance01/01.py
+++ seance01/01.py
@@ -13,7 +13,7 @@ def _():
 @app.cell
 def _(mo):
     mo.md(r"""
-    # Prblème de traversée de rivière
+    # Problème de traversée de rivière
     """)
     return
 
@@ -51,6 +51,14 @@ def _(mo):
     Un état valide est un état qui respecte les contraintes loup/mouton, mouton/choux.
 
     Une arrête est un couple de deux états tel qu'on peut passer du premier au deuxième en une seule traversée de rivière.
+    """)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    ## Implémentation
     """)
     return
 
@@ -95,8 +103,15 @@ def _(Rive, dataclass):
 
 @app.cell
 def _(Etat, Rive):
-    DEPART = Etat(berger=Rive.GAUCHE, loup=Rive.GAUCHE, mouton=Rive.GAUCHE, choux=Rive.DROITE)
+    DEPART = Etat(berger=Rive.GAUCHE, loup=Rive.GAUCHE, mouton=Rive.GAUCHE, choux=Rive.GAUCHE)
     DEPART
+    return (DEPART,)
+
+
+@app.cell
+def _(Etat, Rive):
+    ARRIVEE = Etat(berger=Rive.DROITE, loup=Rive.DROITE, mouton=Rive.DROITE, choux=Rive.DROITE)
+    ARRIVEE
     return
 
 
@@ -172,26 +187,26 @@ def _(mo):
 
 @app.cell
 def _(Etat):
-    def sont_connectes(depart: Etat, arrivee: Etat) -> bool: 
+    def sont_connectes(depart: Etat, arrivee: Etat) -> bool:
         if depart.berger == arrivee.berger:
             return False
         nombre_changements = 0
         if depart.loup != arrivee.loup:
             if depart.loup != depart.berger:
-                return False 
+                return False
             else:
                 nombre_changements = nombre_changements + 1
         if depart.mouton != arrivee.mouton:
             if depart.mouton != depart.berger:
-                return False 
+                return False
             else:
                 nombre_changements = nombre_changements + 1
         if depart.choux != arrivee.choux:
             if depart.choux != depart.berger:
-                return False 
+                return False
             else:
                 nombre_changements = nombre_changements + 1
-        return (nombre_changements < 2)
+        return nombre_changements < 2
     return (sont_connectes,)
 
 
@@ -214,7 +229,74 @@ def _(genere_arretes):
 
 
 @app.cell
-def _():
+def _(mo):
+    mo.md(r"""
+    **BILAN**
+
+    A ce stade on a
+
+    - tous les sommets et les arrêtes du graphe
+    - le sommet DEPART
+    - le sommet ARRIVEE
+
+    On a besoin déterminer un chemin dans ce graphe partant de DEPART et finissant à ARRIVEE.
+    Cet algorithme est indépendant du sens donné aux sommets et aux arrêtes.
+    """)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    ## Méthodes de commodité
+
+    On va ajouter quelques fonctionnalité pour manipuler plus facilement les objets.
+    """)
+    return
+
+
+@app.cell
+def _(Etat, Rive):
+    def visualiser(etat: Etat) -> str:
+        gauche = list()
+        droite = list()
+        if etat.berger == Rive.GAUCHE:
+            gauche.append("B")
+        else:
+            droite.append("B")
+        if etat.loup == Rive.GAUCHE:
+            gauche.append("L")
+        else:
+            droite.append("L")
+        if etat.mouton == Rive.GAUCHE:
+            gauche.append("M")
+        else:
+            droite.append("M")
+        if etat.choux == Rive.GAUCHE:
+            gauche.append("C")
+        else:
+            droite.append("C")
+        return "".join(gauche) + "|" + "".join(droite)
+    return (visualiser,)
+
+
+@app.cell
+def _(DEPART, visualiser):
+    visualiser(DEPART)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    **EXERCICE.** Finir l'implémentation de `etat_from_str` qui fait la démarche inverse de `visualiser_etat`.
+    """)
+    return
+
+
+@app.cell
+def _(Etat):
+    def etat_from_str(chaine: str) -> Etat: ...
     return
 
 
