b7d1752b5659eb89c82fa15a33d1abdbdb43df79
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2026-01-16 14:56:09 +0100

    Ajoute la solution de la séance 03

--- seance03/solution.py
+++ seance03/solution.py
@@ -3,4 +3,15 @@
 Script résolvant le problème de traversée de rivière à l'aide des bibliothèques graphe.py data.py
 """
 
-...
+from graphe import cherche_chemin
+from data import DEPART, ARRIVEE, genere_arretes, visualise_chemin
+
+
+def main():
+    """Fonction principale."""
+    arretes = genere_arretes()
+    print(cherche_chemin(depart=DEPART, arrivee=ARRIVEE, arretes=arretes))
+
+
+if __name__ == "__main__":
+    main()
