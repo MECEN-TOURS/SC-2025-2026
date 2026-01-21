e2cc6f9c5e7ec94d4135fbe67f68c879236df5cb
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2026-01-21 17:48:45 +0100

    Seance 07 ajout module resolution

---
+++ seance07/resolution.py
@@ -0,0 +1,10 @@
+"""Description.
+
+
+Résolution du chemin le plus court entre deux gares connaissant un objet ConnexionsSNCF valide.
+"""
+
+from data import Itineraire, ConnexionsSNCF
+
+
+def resoud(connexions: ConnexionsSNCF, depart: str, arrivee: str) -> ...: ...
