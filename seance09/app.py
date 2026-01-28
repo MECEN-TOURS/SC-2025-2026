5be36e4915dba7734a5585aa944816da28db470b
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2026-01-28 14:46:20 +0100

    Seance 09 exercice ameliorer la visualisation

--- seance09/app.py
+++ seance09/app.py
@@ -9,43 +9,78 @@ def _():
     import marimo as mo
     from data import ConnexionsSNCF, Itineraire
     from resolution import construit_graphe, resoud
-    return (mo,)
-
-
-@app.cell
-def _(mo):
-    mo.md(r"""
-    # Exercice
-
-    Utiliser les éléments d'ui ci-dessous et les fonctions de la bibliothèque pour créer une application permettant de trouver l'itinéraire le plus court entre deux gares.
-    """)
-    return
+    return ConnexionsSNCF, construit_graphe, mo, resoud
 
 
 @app.cell
 def _(mo):
     fichier = mo.ui.file()
     fichier
+    return (fichier,)
+
+
+@app.cell
+def _(ConnexionsSNCF, construit_graphe, fichier):
+    contenu_fichier = fichier.value[0].contents.decode("utf8")
+    sncf = ConnexionsSNCF.model_validate_json(contenu_fichier)
+    graphe = construit_graphe(sncf)
+    gares = list(graphe.nodes)                                         
+    return gares, sncf
+
+
+@app.cell
+def _(gares, mo):
+    gare_depart = mo.ui.dropdown(options=gares)
+    return (gare_depart,)
+
+
+@app.cell
+def _(gares, mo):
+    gare_arrivee = mo.ui.dropdown(options=gares)
+    return (gare_arrivee,)
+
+
+@app.cell
+def _(gare_arrivee, gare_depart, mo):
+    mo.md(f"""
+    gare de départ: {gare_depart}
+
+    gare d'arrivée: {gare_arrivee}
+    """)
     return
 
 
 @app.cell
-def _(mo):
-    menu1 = mo.ui.dropdown(options=["a", "b", "c"], value="a")
-    menu1
-    return (menu1,)
+def _(gare_arrivee, gare_depart, mo, resoud, sncf):
+    bouton = mo.ui.button(
+        on_click=lambda value: resoud(
+                connexions=sncf, 
+                depart=gare_depart.value, 
+                arrivee=gare_arrivee.value, 
+    ), 
+        label="Calcule Trajet"
+    )
+
+    return (bouton,)
 
 
 @app.cell
-def _(menu1):
-    print(menu1.value)
-    return
-
-
-@app.cell
-def _(mo):
-    bouton = mo.ui.button(on_click=lambda _: print("cliqué"), label="Cliquez moi")
+def _(bouton):
     bouton
+    return
+
+
+@app.cell
+def _(bouton):
+    bouton.value
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    **EXERCICE** Reprenez la visualisation du résultat pour avoir un affichage plus lisible
+    """)
     return
 
 
