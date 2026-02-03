9170e7d1cca8c8bee9851a5fd6873a2d48e905d6
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2026-02-03 11:35:58 +0100

    Seance 11 fin de data.py

--- seance11/data.py
+++ seance11/data.py
@@ -28,11 +28,21 @@ class CahierDesCharges(BaseModel):
     @model_validator(mode="after")
     def verifie_noms(self) -> Self:
         """Vérifie que les tâches ont des noms distincts"""
-        ...
+        noms = set()
+        for tache in self.taches:
+            if tache.nom in noms:
+                msg = "Les tâches doivent avoir des noms distincts"
+                raise ValueError(msg)
+            noms.add(tache.nom)
         return self
 
     @model_validator(mode="after")
     def verifie_prerequis(self) -> Self:
         """Vérifie que les prérequis sont bien des tâches du cahier des charges"""
-        ...
+        noms_valides = {tache.nom for tache in self.taches}
+        for tache in self.taches:
+            for prerequis in tache.prerequis:
+                if prerequis not in noms_valides:
+                    msg = "Les prérequis doivent être des tâches valides"
+                    raise ValueError(msg)
         return self
