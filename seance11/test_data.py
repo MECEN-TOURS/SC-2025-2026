7129996fea086735e865b96d0d680361b184b654
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2026-02-03 11:17:52 +0100

    Seance 11 ajout des tests de CahierDesCharges

--- seance11/test_data.py
+++ seance11/test_data.py
@@ -18,7 +18,20 @@ def test_tache_prerequis_circulaire():
         Tache(nom="A", duree=1.0, prerequis=["A", "B"])
 
 
-def test_noms_cahier_des_charges(): ...
+def test_noms_cahier_des_charges():
+    with pytest.raises(ValidationError):
+        CahierDesCharges(
+            taches=[
+                Tache(nom="A", duree=1.0, prerequis=[]),
+                Tache(nom="A", duree=2.0, prerequis=[]),
+            ]
+        )
 
 
-def test_prerequis_cahier_des_charges(): ...
+def test_prerequis_cahier_des_charges():
+    with pytest.raises(ValidationError):
+        CahierDesCharges(
+            taches=[
+                Tache(nom="A", duree=1.0, prerequis=["B"]),
+            ]
+        )
